0e355d51c3bd1f27eb79bae7a04a3b787787a146
ajay <ajay@LAPTOP-MUQ3RMVQ>
2020-03-22 12:45:27 +0530

    Modification

--- stackoverflow/timeSeriesProblem.py
+++ stackoverflow/timeSeriesProblem.py
@@ -8,6 +8,20 @@ data = np.loadtxt(r'data\Bike-Sharing-Dataset\hour.csv',delimiter=",",skiprows=1
 dataset = torch.from_numpy(data)
 print(dataset.shape)  ##1
 d = dataset[:17376,:]
-bikes = d.view((-1,24,17))  ## Batch size is 1, 24 rows and 17 columns
-print(bikes.shape)
 
+
+weather_onehot = d[:,9].to(torch.long)
+print(weather_onehot.shape)
+weather = torch.zeros(weather_onehot.shape[0],4)
+print(weather.shape)
+
+w = weather_onehot.unsqueeze(1)
+
+w = weather.scatter_(1,weather_onehot.unsqueeze(1)-1,1.0)
+print(w.shape)
+
+w = torch.cat((w,d),1)
+print(w)
+
+bikes = w.view((-1,24,21))  ## Batch size is 1, 24 rows and 17 columns
+print(bikes.shape)
