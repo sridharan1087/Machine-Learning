cf577a5debdc934b2c78c1c7e796e89daff191cb
ajay <ajay@LAPTOP-MUQ3RMVQ>
2020-04-03 16:35:26 +0530

    How to use Dataset and DataLoader with simple example

--- huggingFace/pytorchDataSet.py
+++ huggingFace/pytorchDataSet.py
@@ -1,33 +1,35 @@
 import torch
+import pandas as pd
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-import time
+from transformers.tokenization_bert import BertTokenizer
 
 class CustomSet(Dataset):
-    def __init__(self,fileName):
-        with open(fileName) as f:
-            lines = f.read().split('\n')
+    def __init__(self,sentence,label,tokenizer,maxlen):
+        self.sentence = sentence
+        self.label = label
+        self.tokenizer = tokenizer
+        self.maxLen = maxlen
         
-        X=[]
-        y=[] 
-         
-        for line in lines:
-            a = line.split('\t')
-            time.sleep(10)
-            X.append(a[0])
-            y.append(a[1])
-            
-        self.x = X
-        self.y = y
-            
+      
     def __len__(self):
-        return(len(self.y))
+        return len(self.sentence) 
     
     def __getitem__(self, index):
-        return self.x[index],self.y[index]
-            
-            
-obj = CustomSet(r'glue_data\SST-2\dev.tsv')
-dataloader = DataLoader(obj, batch_size = 5, num_workers = 2)
-for i,j in dataloader:
-    print(i)
+        t = self.tokenizer.encode_plus(self.sentence[index],
+                                       max_length=self.maxLen,
+                                       pad_to_max_length=True) 
+        
+        
+        return torch.tensor(t['input_ids'])
+    
+    
+    
+if __name__ == '__main__':
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    df = pd.read_csv(r'C:\Users\ajay\Documents\GitHub\Machine-Learning\huggingFace\glue_data\SST-2\dev.tsv',
+                     delimiter='\t')
+    obj = CustomSet(df['sentence'],df['label'],tokenizer,30)
+    j = DataLoader(obj,batch_size=5,num_workers=2)
+    for i in j:
+        print(i)
